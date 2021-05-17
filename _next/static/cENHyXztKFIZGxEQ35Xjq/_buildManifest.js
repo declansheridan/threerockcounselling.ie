385607d4256b97e2a8ceeae56e1689a654a6ba2d
Declan Sheridan <declan.sheridan@gmail.com>
2021-05-17 12:03:28 +0000

    first export needs to be cleaned up

---
+++ _next/static/cENHyXztKFIZGxEQ35Xjq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9140b3201915661a0922.js"],"/_error":["static/chunks/pages/_error-a0d05e04724de07befd6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
