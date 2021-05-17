0f735b6b290361f2fa8cb0ba5cb1175f0c1682a5
Declan Sheridan <declan.sheridan@gmail.com>
2021-05-17 21:39:16 +0000

    Update

---
+++ _next/static/wWDmnQK4IhBmdJZf_SKOX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-42452d81b3a14d930080.js"],"/_error":["static/chunks/pages/_error-a0d05e04724de07befd6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
