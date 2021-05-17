2080cae37e4fb2caaade239f61163932a53e229c
Declan Sheridan <declan.sheridan@gmail.com>
2021-05-17 17:20:21 +0000

    Update

--- _next/static/dwpisSJCk5VVfI5FK2Reg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9140b3201915661a0922.js"],"/_error":["static/chunks/pages/_error-a0d05e04724de07befd6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
